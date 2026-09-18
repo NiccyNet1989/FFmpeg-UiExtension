89eb811210899a317f4f3375c2a92d03e463d09f
NiccyNet1989 <nicholasforge1989@yahoo.com>
2026-09-17 20:41:11 -0700

    Created visuals for 2nd Panel for MP4 > PNG Sequence
    Finished up the 2nd panel's UI appearance. Currently still lacks Action Editors and connection to FFmpeg, but will add once UI is in a desirable visual state.

--- UI Extension (Java)/src/UserInterface.java
+++ UI Extension (Java)/src/UserInterface.java
@@ -27,10 +27,11 @@ public class UserInterface {
 
         // Basic frame components
         this.frame = new JFrame("User Interface");
-        frame.setLayout(new GridLayout(0,1));
+        frame.setLayout(new GridLayout(0, 1));
         GridBagConstraints constraints = new GridBagConstraints();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(800, 550));
+        frame.setSize(new Dimension(600, 400));
+        frame.setResizable(false);
 
 
         // Defining the three panels main panels where the components will reside
@@ -65,7 +66,79 @@ public class UserInterface {
 
         JLabel filePathLabel = new JLabel("File Path");
         constraints = new GridBagConstraints();
-        constraints.gridx = 1;          // X Position in grid
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;
+        constraints.ipady = 0;
+        constraints.insets = new Insets(10, 10, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel1.add(filePathLabel, constraints);
+
+        JTextField filePathTextField = new JTextField();
+        filePathTextField.setPreferredSize(new Dimension(300, 25));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 3;
+        constraints.gridheight = 1;
+        constraints.weightx = 0.5;
+        constraints.weighty = 0.1;
+        constraints.insets = new Insets(0, 10, 50, 0);
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel1.add(filePathTextField, constraints);
+
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;      // Position in grid
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;  // Scale of component
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;  // Adjusts how much empty space this component is given when window is resized
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;      // Attempts to manually resize the component
+        constraints.ipady = 0;
+        constraints.insets = new Insets(0, 0, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.LINE_START;
+        panel1.add(new JPanel(), constraints);
+
+        JLabel frameSpinnerLabel = new JLabel("Frames per PNG Extraction");
+        constraints = new GridBagConstraints();
+        constraints.gridx = 2;      // Position in grid
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;  // Scale of component
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;  // Adjusts how much empty space this component is given when window is resized
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;      // Attempts to manually resize the component
+        constraints.ipady = 0;
+        constraints.insets = new Insets(10, 80, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel1.add(frameSpinnerLabel, constraints);
+
+        SpinnerModel numberSpinnerModel = new SpinnerNumberModel(1,1,200,1); // (Initial, min, max, step)
+        JSpinner frameSpinner = new JSpinner(numberSpinnerModel);
+        constraints = new GridBagConstraints();
+        constraints.gridx = 2;      // Position in grid
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;  // Scale of component
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;  // Adjusts how much empty space this component is given when window is resized
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;      // Attempts to manually resize the component
+        constraints.ipady = 0;
+        constraints.insets = new Insets(0, 80, 50, 0);
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel1.add(frameSpinner, constraints);
+
+
+        //==================================================
+        // Part 3 - Panel 2
+        JLabel outputFolderLabel = new JLabel("Output Folder Name");
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;          // X Position in grid
         constraints.gridy = 0;          // Y Position in grid
         constraints.gridwidth = 1;      // X scale of component
         constraints.gridheight = 1;     // Y scale of component
@@ -73,28 +146,48 @@ public class UserInterface {
         constraints.weighty = 0.1;
         constraints.ipadx = 0;
         constraints.ipady = 0;
-        constraints.insets = new Insets(10,10,0,0);   // Insets = (Top, Left, Bottom, Right)
-        constraints.anchor = GridBagConstraints.LINE_START;
-        panel1.add(filePathLabel, constraints);
+        constraints.insets = new Insets(10, 10, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel2.add(outputFolderLabel, constraints);
 
-        JTextField filePathTextField = new JTextField();
-        filePathTextField.setPreferredSize(new Dimension(300, 25));
+        JTextField outputFolderTextField = new JTextField();
+        outputFolderTextField.setPreferredSize(new Dimension(300, 25));
         constraints = new GridBagConstraints();
-        constraints.gridx = 1;
+        constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.gridwidth = 3;
         constraints.gridheight = 1;
         constraints.weightx = 0.5;
         constraints.weighty = 0.1;
-        constraints.insets = new Insets(0,10,0,0);
+        constraints.insets = new Insets(0, 10, 50, 0);
         constraints.anchor = GridBagConstraints.FIRST_LINE_START;
-        panel1.add(filePathTextField, constraints);
+        panel2.add(outputFolderTextField, constraints);
 
-        //==================================================
-        // Part 3 - Panel 2
-        JTextField outputFolderName = new JTextField("Output Folder Name");
-//        panel2.add(outputFolderName);
+        constraints = new GridBagConstraints();
+        constraints.gridx = 2;      // Position in grid
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;  // Scale of component
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;  // Adjusts how much empty space this component is given when window is resized
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;      // Attempts to manually resize the component
+        constraints.ipady = 0;
+        constraints.insets = new Insets(0, 0, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.LINE_START;
+        panel2.add(new JPanel(), constraints);
 
+        constraints = new GridBagConstraints();
+        constraints.gridx = 2;      // Position in grid
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;  // Scale of component
+        constraints.gridheight = 1;
+        constraints.weightx = 0.1;  // Adjusts how much empty space this component is given when window is resized
+        constraints.weighty = 0.1;
+        constraints.ipadx = 0;      // Attempts to manually resize the component
+        constraints.ipady = 0;
+        constraints.insets = new Insets(0, 0, 0, 0);   // Insets = (Top, Left, Bottom, Right)
+        constraints.anchor = GridBagConstraints.LINE_START;
+        panel2.add(new JPanel(), constraints);
 
         //==================================================
         // Part 4 - Panel 3
