3342c63dbf155c49a2fecba5e9e5799ac51615db
NiccyNet1989 <nicholasforge1989@yahoo.com>
2026-09-18 16:27:01 -0700

    Making progress with console interfacing
    Was able to successfully run FFmpeg and convert  a simple test file into a PNG Sequence via Java code inputted into Driver.java
    
    Before connecting this to the UI, the following two issues need to be resolved:
    1. The execution of the command did NOT naturally terminate once finished. Upon doing the exact same command in a cmd prompt opened up via File Explorer, FFmpeg naturally finishes the process and the console continues as expected. There is likely an issue with the ConsoleBridge code here causing this!
    
    2. To get this to work, we had to copy-paste FFmpeg.exe itself into the root folder of the entire project, which is a working solution but isn't cohesive with the desired architecture. All of the content that I am making should go into its respective Java UI folder, and all the other content made by Fabrice Bellard and BtbN should be in its own separate space! We need to figure out how to use the ffmpeg command in the console prompt while in a command line in the root directory! (Note: The copy-pasted ffmpeg has been removed to avoid needing to use Git LFS a second time, but it was being used during testing)

--- UI Extension (Java)/src/ConsoleBridge.java
+++ UI Extension (Java)/src/ConsoleBridge.java
@@ -2,37 +2,43 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 //This class is designed to bridge input between the Java program and the console
 public class ConsoleBridge {
-    String commandLineInput;
+    String[] commandLineInput;
     Scanner scanner;
     File directory;
+    ProcessBuilder processBuilder;
+    Process process;
+    OutputStream outputStream;
+    InputStream inputStream;
+    InputStream stderror;
+
 
     public ConsoleBridge() {
-        this.commandLineInput = "";
+        this.commandLineInput = new String[]{""};
         this.scanner = new Scanner(System.in);
         this.directory = new File(System.getProperty("user.dir").toString());
 
 
         while (!commandLineInput.equals("exit")) {
             System.out.print("> ");
-            this.commandLineInput = scanner.nextLine();
+            this.commandLineInput = new String[]{scanner.nextLine()};
 
-//            String[] commandBuild = {"cmd.exe", "mkdir test", System.getProperty("user.dir").toString()};
-
-            ProcessBuilder processBuilder = new ProcessBuilder();
+            this.processBuilder = new ProcessBuilder();
             processBuilder.directory(this.directory);
-            processBuilder.command("cmd.exe", "/c", commandLineInput);
+//            processBuilder.command("cmd.exe", "/c", this.commandLineInput);
+            processBuilder.command(this.commandLineInput);
 
             try {
-                Process process = processBuilder.start();
+                this.process = processBuilder.start();
 
-                OutputStream outputStream = process.getOutputStream();
-                InputStream inputStream = process.getInputStream();
-                InputStream stderror = process.getErrorStream();
+                this.outputStream = process.getOutputStream();
+                this.inputStream = process.getInputStream();
+                this.stderror = process.getErrorStream();
 
                 process.waitFor(5, TimeUnit.SECONDS);
 
@@ -44,6 +50,36 @@ public class ConsoleBridge {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+
+    // The below constructor is intended to be used for one-time command console calls, and is what the UI will primarily be using
+    public ConsoleBridge(String[] inputtedCommand, Path inputtedDirectory) {
+        this.commandLineInput = inputtedCommand;
+//        this.directory = new File(System.getProperty("user.dir").toString());
+        this.directory = inputtedDirectory.toFile();
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.directory(this.directory);
+        processBuilder.command(this.commandLineInput);
+
+        try {
+            this.process = processBuilder.start();
+
+            this.outputStream = process.getOutputStream();
+            this.inputStream = process.getInputStream();
+            this.stderror = process.getErrorStream();
+
+            if (process.waitFor() == 0) {
+                outputStream.flush();
+                outputStream.close();
+                process.destroy();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- UI Extension (Java)/src/Driver.java
+++ UI Extension (Java)/src/Driver.java
@@ -4,10 +4,19 @@ import java.nio.file.Paths;
 public class Driver {
     public static void main(String[] args) {
 //        UserInterface ui = new UserInterface();
-        Path FFmpegPath = Paths.get(System.getProperty("user.dir")).getParent();
-        FFmpegPath = FFmpegPath.resolve("bin").resolve("ffmpeg.exe");
-        System.out.print(FFmpegPath);
+//        Path FFmpegPath = Paths.get(System.getProperty("user.dir")).getParent();
+//        FFmpegPath = FFmpegPath.resolve("bin");
+//        System.out.print(FFmpegPath);
 
-        ConsoleBridge consoleBridge = new ConsoleBridge();
+        String fileName = "\"Test Gif\"";
+        String mkdirCommand = "mkdir " + fileName;
+        System.out.print(mkdirCommand);
+
+//        String[] ffmpegCommand = {"cmd.exe", "/c", "ffmpeg", "-i", "Test Gif.mp4", "Test Gif/Test Gif_%04d.png"};
+
+        String[] commandInput = {"cmd.exe", "/c", "mkdir test"};
+        ConsoleBridge consoleBridge = new ConsoleBridge(commandInput, Paths.get(System.getProperty("user.dir")).getParent());
+
+        
     }
 }
